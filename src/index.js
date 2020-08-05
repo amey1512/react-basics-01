99460a6a669e92ee47e6aae958792056421e3168
amey <amey1512@gmail.com>
2020-08-06 03:24:31 +0530

    created small const component.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return  <h1> Post React Setup!!! 
+            <h3> It is good to learn, REACT from scratch</h3>
+          </h1>
+};
+
+ReactDOM.render(<App/>, document.getElementById('root'));
